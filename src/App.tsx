f7bcfb929e89254d05e39837ea0cb4f981e9c668
Abhishek Govindarasu <abhishekgovindarasu@gmail.com>
2025-11-04 22:42:00 -0800

    ui

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,19 @@ import Home from './components/Home';
 import View from './components/View';
 import { isValidUUID } from './lib/utils';
 
-
 const App = () => {
   const path = window.location.pathname.endsWith('/') ? window.location.pathname.slice(0, -1) : window.location.pathname;
+  
   if (isValidUUID(path.slice(1))) {
     return <View hash={path.slice(1)} />;
-  } else if (path === '') {
-    return <Home />;
-  } else {
-    window.location.replace('/');
   }
+  
+  if (path === '') {
+    return <Home />;
+  }
+  
+  window.location.replace('/');
   return null;
-}
+};
+
 export default App;
